3671031d1f4f053135dce00ec9911c216d4e77bd
Yatzary <nicolesantanam@hotmail.com>
2020-03-31 19:52:59 -0400

    se agrego el modulo citas

---
+++ ProyectoFinal/ProyectoFinal/Controllers/CitasController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinal.Models;
+
+namespace ProyectoFinal.Controllers
+{
+    public class CitasController : Controller
+    {
+        private HospitalContext db = new HospitalContext();
+
+        // GET: Citas
+        public ActionResult Index()
+        {
+            var citas = db.Citas.Include(c => c.Medicos).Include(c => c.Pacientes);
+            return View(citas.ToList());
+        }
+
+        // GET: Citas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Citas citas = db.Citas.Find(id);
+            if (citas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(citas);
+        }
+
+        // GET: Citas/Create
+        public ActionResult Create()
+        {
+            ViewBag.Medico_Id = new SelectList(db.Medicos, "IdMedicos", "Nombre");
+            ViewBag.Paciente_Id = new SelectList(db.Pacientes, "IdPacientes", "Cedula");
+            return View();
+        }
+
+        // POST: Citas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdCitas,Paciente_Id,Medico_Id,Fecha,Hora")] Citas citas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Citas.Add(citas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Medico_Id = new SelectList(db.Medicos, "IdMedicos", "Nombre", citas.Medico_Id);
+            ViewBag.Paciente_Id = new SelectList(db.Pacientes, "IdPacientes", "Cedula", citas.Paciente_Id);
+            return View(citas);
+        }
+
+        // GET: Citas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Citas citas = db.Citas.Find(id);
+            if (citas == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Medico_Id = new SelectList(db.Medicos, "IdMedicos", "Nombre", citas.Medico_Id);
+            ViewBag.Paciente_Id = new SelectList(db.Pacientes, "IdPacientes", "Cedula", citas.Paciente_Id);
+            return View(citas);
+        }
+
+        // POST: Citas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdCitas,Paciente_Id,Medico_Id,Fecha,Hora")] Citas citas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(citas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Medico_Id = new SelectList(db.Medicos, "IdMedicos", "Nombre", citas.Medico_Id);
+            ViewBag.Paciente_Id = new SelectList(db.Pacientes, "IdPacientes", "Cedula", citas.Paciente_Id);
+            return View(citas);
+        }
+
+        // GET: Citas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Citas citas = db.Citas.Find(id);
+            if (citas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(citas);
+        }
+
+        // POST: Citas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Citas citas = db.Citas.Find(id);
+            db.Citas.Remove(citas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
